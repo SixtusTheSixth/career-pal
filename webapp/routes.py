afb231df4b8e7cdab0ccc0c1b2e656f5fb2e37fb
SixtusTheSixth <crossbiblewine@gmail.com>
2023-06-13 19:06:39 -0400

    trying to make git work pt 2

--- webapp/routes.py
+++ webapp/routes.py
@@ -4,17 +4,4 @@ from flask import render_template
 @webapp.route('/')
 @webapp.route('/index')
 def index():
-	'''
-	user = {'username': 'Aquila'}
-	posts = [
-		{
-			'author': {'username' : 'John'},
-			'body': 'Beatiful day in Portland!'
-		},
-		{
-			'author': {'username': 'Susan'},
-			'body': 'The Avengers movie was so cool!'
-		}
-	]
-	'''
 	return render_template('index.html', title='CareerPal')
